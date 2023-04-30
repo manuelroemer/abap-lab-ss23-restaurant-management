8beb1e47f7fe7160c5acc4270825dee609438811
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2023-04-30 13:05:13 +0200

    Initial commit.

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,7 @@
+sap.ui.define(['sap/ui/core/mvc/Controller'], function (BaseController) {
+  'use strict';
+
+  return BaseController.extend('restaurant00045.controller.App', {
+    onInit() {},
+  });
+});
