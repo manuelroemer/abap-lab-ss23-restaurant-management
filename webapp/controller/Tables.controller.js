d99ee88311b621d011c17aefde5b16cc802b0c53
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2023-05-07 16:50:57 +0200

    Skeleton for tables view.

---
+++ webapp/controller/Tables.controller.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+  ['restaurant00045/controller/BaseController'],
+  /**
+   * @param {typeof restaurant00045.controller.BaseController} BaseController
+   */
+  (BaseController) => {
+    class TablesController extends BaseController {
+    }
+
+    return TablesController;
+  },
+);
