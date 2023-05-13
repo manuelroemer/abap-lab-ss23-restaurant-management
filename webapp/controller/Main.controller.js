c157997df82df57479892dad309c14f7e9691855
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2023-05-13 19:03:03 +0200

    Hack together an edit reg dialog-

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,9 +1,9 @@
 sap.ui.define(
-  ['restaurant00045/controller/BaseController'],
+  ['restaurant00045/controller/BaseController', 'sap/m/MessageBox'],
   /**
    * @param {typeof restaurant00045.controller.BaseController} BaseController
    */
-  (BaseController) => {
+  (BaseController, MessageBox) => {
     class MainController extends BaseController {
       onBookPress() {
         this.router.navTo('Book');
@@ -11,6 +11,36 @@ sap.ui.define(
 
       onAdminPress() {
         this.router.navTo('Admin');
+      }
+
+      onBookEditPress() {
+        const dialog = this.byId('bookEditDialog');
+        dialog.open();
+      }
+
+      onBookEditDialogSubmit() {
+        const reservationId = this.byId('bookEditDialogReservationId').getValue();
+        if (!reservationId) {
+          return;
+        }
+
+        if (reservationId) {
+          this.svc.read(`/ReservationSet('${reservationId}')`, {
+            success: () => {
+              this.byId('bookEditDialogReservationId').setValue('');
+              this.byId('bookEditDialog').close();
+              this.router.navTo('Book', { reservationId });
+            },
+            error: () => {
+              MessageBox.warning('Sorry, we could not find a reservation with that ID. Please verify that it exists and then try again. Hint: You can see all IDs in the Administration overview.');
+            },
+          });
+        }
+      }
+
+      onBookEditDialogCancel() {
+        this.byId('bookEditDialogReservationId').setValue('');
+        this.byId('bookEditDialog').close();
       }
     }
 
